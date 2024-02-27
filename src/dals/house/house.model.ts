9e343e1213fb7797c4f0ca9be3bf65dc1ba6217a
Abel Tovar <abel@tovar.es>
2024-02-27 17:01:39 +0100

    Add House Model

---
+++ src/dals/house/house.model.ts
@@ -0,0 +1,11 @@
+export interface House {
+  id: string;
+  title: string;
+  image: string;
+  description: string;
+  address: string;
+  roomsNumber: number;
+  bedsNumber: number;
+  bathsNumber: number;
+  lastReviews: [];
+}
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,11 @@
 import "./core/load-env";
-import  {  Request, Response } from "express";
 import { envConstants } from "./core/constants";
 import { createRestApiServer } from "./core/servers";
 
 const app = createRestApiServer();
 const port = envConstants.PORT || 3000;
 
-app.get("/", (req: Request, res: Response) => {
+app.get("/", (req, res) => {
   res.send("Express + Typescript server started...");
 });
 
